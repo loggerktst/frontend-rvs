62eace32f7b3be006cda993f37faa657414673c3
lucas menezes <lucas-rdm@hotmail.com>
2020-11-29 20:39:25 -0300

    add cadastro cliente

---
+++ cadastrocliente.js
@@ -0,0 +1,94 @@
+function preencheradvogados(lista) {
+    var saida = "<option value ='0'>Selecione um Advogado...</option>";
+
+    for (cont = 0; cont < lista.length; cont++) {
+        saida +=
+            "<option value='" + lista[cont].idAdvogado + "'>" + lista[cont].nomeAdvogado + "</option>";
+			window.alert(lista[cont].idadvogado)
+    }
+    document.getElementById("idadvogado").innerHTML = saida;
+}
+
+function carregaradvogados() {
+    fetch("http://localhost:8080/listaadvogados")
+        .then(res => res.json())
+        .then(res => preencheradvogados(res));
+}
+
+
+function gravar() {
+
+    if (document.getElementById("nomecli").value.length > 0 
+        && document.getElementById("emailcli").value.length > 0 
+        && document.getElementById("celcli").value.length >0
+
+		
+		){
+		
+		
+			if(validaprocesso()==true || document.getElementById("idprocesso").value == 0){
+
+					var objeto = {
+						nomecli: document.getElementById("nomecli").value,
+						emailcli: document.getElementById("emailcli").value,
+						celcli: document.getElementById("celcli").value,
+						foto: document.getElementById("foto").value
+
+				   
+					}
+
+					var cabecalho = {
+						method: "POST",
+						body: JSON.stringify(objeto),
+						headers: {
+							"Content-type": "application/json"
+						}
+					}
+
+					fetch("http://localhost:8080/novocliente", cabecalho)
+						.then(res => res.json())
+						.then(res => { window.alert("Gravado com sucesso") })
+						.catch(err => { window.alert("ocorreu um erro") });
+						window.location = "cadastrocliente.html";
+
+				 
+			}
+			
+			else{
+				document.getElementById("alertdata").innerHTML =
+				"<div class='alert alert-danger' role='alert'> Preencha todos os campos </div>";
+			}
+			
+			
+
+		} else {
+			document.getElementById("alertdata").innerHTML =
+				"<div class='alert alert-danger' role='alert'> Processo nao encontrado </div>";
+			document.getElementById("idprocesso").focus;
+
+		}
+}
+
+
+function preencheprocessos(lista) {
+
+    for (cont = 0; cont < lista.length; cont++) {
+		
+		if(lista[cont].idprocesso==document.getElementById("idadvogado")){
+			return true;
+		}
+    }
+	return false;
+    
+}
+
+function validaprocesso() {
+    fetch("http://localhost:8080/listaprocessos")
+        .then(res => res.json())
+        .then(res => preencheprocessos(res));
+	return res;
+}
+
+
+
+
